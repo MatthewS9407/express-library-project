aa32e6cad5078b2cb008c8e5323d9269446ef6d4
Matthew Schröder <matthewschroeder94@gmail.com>
2022-10-03 17:06:18 +0200

    add create, read, update functionality

---
+++ routes/book.routes.js
@@ -0,0 +1,93 @@
+const Book = require("../models/Book.model");
+
+const router = require("express").Router();
+
+//READ: List of all books
+router.get("/books", (req, res, next) => {
+  Book.find()
+    .then( booksFromDB => {
+        res.render("books/books-list", {books: booksFromDB})
+    })
+    .catch( err => {
+        console.log("error getting books from DB", err);
+        next();
+    })
+});
+
+
+//READ: Book details
+router.get("/books/:bookId", (req, res, next) => {
+    const id = req.params.bookId;
+
+    Book.findById(id)
+    .then(bookDetails => {
+        res.render("books/book-details", bookDetails)
+    })
+    .catch( err => {
+        console.log("error getting book details fom DB", err);
+        next();
+    })
+});
+
+//CREATE: display form
+router.get("/books/create", (req, res, next) => {
+    res.render("books/book-create");
+})
+
+//CREATE: process form
+router.post("/books/create", (req, res, next) => {
+    const bookDetails = {
+        title: req.body.title,
+        description: req.body.description,
+        author: req.body.author,
+        rating: req.body.rating,
+    }
+
+    Book.create(bookDetails)
+    .then( bookDetails => {
+        res.redirect("/books")
+    })
+    .catch(err => {
+        console.log("error creating new book in DB", err);
+        next();
+    })
+
+})
+
+
+//UPDATE: display form
+router.get("/books/:bookId/edit", (req, res, next) => {
+    Book.findById(req.params.bookId)
+      .then( (bookDetails) => {
+        res.render("books/book-edit", bookDetails);
+      })
+      .catch( err => {
+        console.log("Error getting book details from DB...", err);
+        next();
+      });
+  });
+
+
+  //UPDATE: process form
+router.post("/books/:bookId/edit", (req, res, next) => {
+    const bookId = req.params.bookId;
+  
+    const newDetails = {
+      title: req.body.title,
+      author: req.body.author,
+      description: req.body.description,
+      rating: req.body.rating,
+    }
+  
+    Book.findByIdAndUpdate(bookId, newDetails)
+      .then( () => {
+        res.redirect(`/books/${bookId}`);
+      })
+      .catch( err => {
+        console.log("Error updating book...", err);
+        next();
+      });
+  });
+  
+
+module.exports = router;
